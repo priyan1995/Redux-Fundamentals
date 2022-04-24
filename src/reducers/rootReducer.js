fb93ac9825d7f1407f9e9590e1b0df6afc5a99ed
priyan <45042030+priyan1995@users.noreply.github.com>
2022-04-24 19:01:19 +0530

    redux package added

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,27 @@
+const initialState = {
+    posts: [
+        {
+            id:'1', 
+            title: 'Lorem Ipsum', 
+            body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+        },
+        {
+            id:'2', 
+            title: 'Finibus Bonorum et Malorum', 
+            body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. '
+        },
+        {
+            id:'3', 
+            title: 'occaecati cupiditate non provident', 
+            body: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident. '
+        }
+    ]
+} 
+
+
+
+const rootReducer = (state = initialState, action) => {
+    return state;
+}
+
+export default rootReducer;
