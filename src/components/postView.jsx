0484ee437b5f664e1cf2953d631ffc7531f7f8fe
PriyanIH <priyan.abewickrama@ideahubtech.com>
2022-04-26 14:22:52 +0530

    dispatch action

--- src/components/postView.jsx
+++ src/components/postView.jsx
@@ -1,12 +1,29 @@
 import { connect } from "react-redux"
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const PostView = (props) => {
+
+    
+
     const postItem = props.posts;
-    console.log(postItem);
+    //console.log(props);
+
+    let history = useHistory();
+    const deleteHandler = () => {
+       
+        props.deletePost(props.posts.id);
+
+        
+        //history.push('/');
+       
+    }
+
+
     return (
         <>
             <h2>{postItem.title}</h2>
             <p>{postItem.body}</p>
+            <button onClick={deleteHandler}>Delete</button>
         </>
     )
 
@@ -14,9 +31,18 @@ const PostView = (props) => {
 
 const mapStateToProps = (state, ownProps) => {
     let pid = ownProps.match.params.postId;
-    return{
+    return {
         posts: state.posts.find(posts => posts.id === pid)
     }
 }
 
-export default connect(mapStateToProps)(PostView)
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deletePost: (id) => { dispatch({ type: 'DELETE_POST', id: id }) }
+    }
+}
+
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostView)
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -34,7 +34,23 @@ const initialState = {
 
 
 const rootReducer = (state = initialState, action) => {
+    // console.log(action)
+
+    if(action.type === 'DELETE_POST'){
+        let newPosts = state.posts.filter( post => {
+            return action.id !== post.id
+        });
+
+        return{
+            ...state,
+            posts: newPosts
+            
+        }
+        
+    }
+
     return state;
+  
 }
 
 export default rootReducer;
