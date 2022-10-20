e5068c75232f57fafdbf84548e776cedfbcb3252
priyan <priyan.abewickrama@ideahub.lk>
2022-10-20 10:23:01 +0530

    data array created

---
+++ src/dataArrays/CountriesData.js
@@ -0,0 +1,27 @@
+export const CountriesData = [
+    {
+        id: '1',
+        name: 'United States',
+        population: '329.5 million',
+        capital: 'Washington, D.C.'
+    },
+    {
+        id: '2',
+        name: 'India',
+        population: '1.38 billion',
+        capital: 'New Delhi'
+    },
+    {
+        id: '3',
+        name: 'United Kingdom',
+        population: '67.22 million',
+        capital: 'London'
+    },
+    {
+        id: '4',
+        name: 'Sri Lanka',
+        population: '21.92 million',
+        capital: 'Colombo'
+    }
+
+]
