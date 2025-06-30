db8ce885f3034c184bab806fd7ff2d3e60a79487
priyan1995 <priyan.abewickrama@ideahub.lk>
2025-06-30 15:58:27 +0530

    fetch book data from API

--- src/components/pages/books/Books.jsx
+++ src/components/pages/books/Books.jsx
@@ -6,17 +6,35 @@ export const Books = () => {
 
     const dispatch = useDispatch();
     const booksList = useSelector((state) => state.books.list);
+    const isLoading = useSelector((state) => state.books.loading);
+    const error = useSelector((state)=>state.books.error);
 
     useEffect(() => {
         dispatch(fetchBooks())
     },
         [dispatch])
 
-    console.log(booksList);
+
+    if (isLoading) {
+        return <h2>Loading...</h2>
+    }
+
+    if(error){
+        return <h2>Something went wrong...</h2>
+    }
 
     return (
         <>
             <h2>Books List</h2>
+
+            {
+                booksList.map((book, index) => (
+                    <div className="pd-book-item" key={index}>
+                        <p><strong>Name : </strong>{book.title}</p>
+                        <p><strong>Description : </strong>{book.body}</p>
+                    </div>
+                ))
+            }
         </>
     )
 }
